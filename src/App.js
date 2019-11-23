4bdfa690740849aa945d627d4dce06a6170501d4
Brandon.Davison@wsu.edu <bdavison@eecs.wsu.edu>
2019-11-23 12:58:01 -0800

    almost working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LikeButton from './Components/LikeButton'
+//import LikeButton from './Components/LikeButton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Some random React App</h3>
+      <LikeButton></LikeButton>
     </div>
   );
 }
---
+++ src/Components/LikeButton.js
@@ -0,0 +1,26 @@
+import React from 'react';
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e('button', { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+export default LikeButton;
+
+// const domContainer = document.querySelector('#like_button_container');
+// ReactDOM.render(e(LikeButton), domContainer);
