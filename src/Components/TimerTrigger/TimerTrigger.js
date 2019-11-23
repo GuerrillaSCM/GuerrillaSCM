d22c57c0b9b212c61a8a0924fae04f0c85d284a8
Brandon.Davison@wsu.edu <bdavison@eecs.wsu.edu>
2019-11-23 14:05:44 -0800

    Count up timer implemented

--- src/Components/TimerTrigger/TimerTrigger.js
+++ src/Components/TimerTrigger/TimerTrigger.js
@@ -2,57 +2,28 @@ const React = require('react')
 const ms = require('pretty-ms')
 
 class TimerTrigger extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      time: 0,
-      isOn: false,
-      start: 0
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: 0,
+            start: Date.now()
+        }
+        this.startTimer();
     }
-    this.startTimer = this.startTimer.bind(this)
-    this.stopTimer = this.stopTimer.bind(this)
-    this.resetTimer = this.resetTimer.bind(this)
-  }
-  startTimer() {
-    this.setState({
-      isOn: true,
-      time: this.state.time,
-      start: Date.now() - this.state.time
-    })
-    this.timer = setInterval(() => this.setState({
-      time: Date.now() - this.state.start
-    }), 1);
-  }
-  stopTimer() {
-    this.setState({isOn: false})
-    clearInterval(this.timer)
-  }
-  resetTimer() {
-    this.setState({time: 0, isOn: false})
-  }
-  render() {
-    let start = (this.state.time == 0) ?
-      <button onClick={this.startTimer}>start</button> :
-      null
-    let stop = (this.state.time == 0 || !this.state.isOn) ?
-      null :
-      <button onClick={this.stopTimer}>stop</button>
-    let resume = (this.state.time == 0 || this.state.isOn) ?
-      null :
-      <button onClick={this.startTimer}>resume</button>
-    let reset = (this.state.time == 0 || this.state.isOn) ?
-      null :
-      <button onClick={this.resetTimer}>reset</button>
-    return(
-      <div>
-        <h3>timer: {ms(this.state.time)}</h3>
-        {start}
-        {resume}
-        {stop}
-        {reset}
-      </div>
-    )
-  }
+
+    startTimer() {
+        this.timer = setInterval(() => this.setState({
+            time: Date.now() - this.state.start
+        }), 1);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Time elapsed: {ms(this.state.time)}</h3>
+            </div>
+        );
+    }
 }
 
 export default TimerTrigger;
