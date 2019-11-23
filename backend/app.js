a79135fb88de7dea1f9b90e5db37f7d0a53ef92c
Isaac <isaac.schultz@wsu.edu>
2019-11-23 12:14:36 -0800

    Implementing Mongoose and generating schema

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,52 @@
     File: server.js
 */
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/meme/', function (req, res) {
-    console.log("This bitch aint emtpy, anti-yeet!")
-  res.send('yeetus deletus')
-})
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://testing:oeXeGlFbH8U1uEjA@guerrillascm-rk5d5.mongodb.net/GuerrillaSCM?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true
+});
+ 
+// get reference to database
+var db = mongoose.connection;
+ 
+db.on('error', console.error.bind(console, 'connection error:'));
+ 
+db.once('open', function() {
+    console.log("Connection Successful!");
+    
+    // define Schema
+    var MemeMarket = mongoose.Schema({
+      name: String,
+      price: Number,
+      quantity: Number
+    });
+ 
+    // compile schema to model
+    var Meme = mongoose.model('Meme', MemeMarket, 'Survey');
+ 
+    // a document instance
+    var meme1 = new Meme({ name: 'Admantium pepe', price: 10, quantity: 25 });
+ 
+    // save model to database
+    meme1.save(function (err, meme) {
+      if (err) return console.error(err);
+      console.log(meme.name + " saved to meme collection.");
+    });
+    
+}); 
+
 
 app.listen(3000); //Listens for requests (asynchronous!)
 
-console.log('djfgdksfadskjfahlksdhflkahsdlkjfhalksdhf API running on port: ' + 3000);
+console.log('API running on port: ' + 3000);
+
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/meme/', function (req, res) {
+  console.log("This bitch aint emtpy, anti-yeet!")
+  res.send('yeetus deletus')
+})
