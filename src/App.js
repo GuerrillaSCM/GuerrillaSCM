a4951d05d837b6b3f72b9e8381b631cfd5dd6aa7
Brandon.Davison@wsu.edu <bdavison@eecs.wsu.edu>
2019-11-23 13:44:10 -0800

    Working timer clock

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import LikeButton from './Components/LikeButton/LikeButton'
-//import LikeButton from './Components/LikeButton';
+import TimerTrigger from './Components/TimerTrigger/TimerTrigger';
 
 function App() {
   return (
     <div className="App">
       <h3>Some random React App</h3>
       <LikeButton></LikeButton>
+      <TimerTrigger></TimerTrigger>
     </div>
   );
 }
