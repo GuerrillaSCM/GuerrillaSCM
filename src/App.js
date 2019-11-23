d98e2b85cd9384fe32df554e796c62264729cc10
Brandon.Davison@wsu.edu <bdavison@eecs.wsu.edu>
2019-11-23 13:10:07 -0800

    Embeded stateless component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LikeButton from './Components/LikeButton'
 //import LikeButton from './Components/LikeButton';
--- src/Components/LikeButton.js
+++ src/Components/LikeButton.js
@@ -1,24 +1,31 @@
 import React from 'react';
-'use strict';
 
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e('button', { onClick: () => this.setState({ liked: true }) },
-      'Like'
+const LikeButton = () => {
+    return (
+        <div>
+            <p>This is from React</p>
+        </div>
     );
-  }
 }
+
+// const e = React.createElement;
+
+// class LikeButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { liked: false };
+//   }
+
+//   render() {
+//     if (this.state.liked) {
+//       return 'You liked this.';
+//     }
+
+//     return e('button', { onClick: () => this.setState({ liked: true }) },
+//       'Like'
+//     );
+//   }
+// }
 
 export default LikeButton;
 
