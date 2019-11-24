76134ac8d067a1fe9d5c1129357bbea81ba399a3
Josh <josh.lapierre@wsu.edu>
2019-11-23 17:52:00 -0800

    removed db file

--- backend/app.js
+++ backend/app.js
@@ -15,23 +15,6 @@ const memeRoutes = require('./routes/meme');
 const mongoose = require('mongoose');
 
 
-
-const uri = "mongodb+srv://testing:oeXeGlFbH8U1uEjA@guerrillascm-rk5d5.mongodb.net/GuerrillaSCM?retryWrites=true&w=majority";
-mongoose.connect(uri, {
-  useNewUrlParser: true
-});
-
-// get reference to database
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-app.locals.db = db; // save the databse somehwere that it can be accessed globally if needed. Not ideal!
-
-db.once('open', function () {
-  console.log("Connection Successful!");
-});
-
 // Routing API end point assignments
 app.use('/api/response/', responseRoutes);
 app.use('/api/survey/', surveyRoutes);
@@ -42,6 +25,14 @@ app.use((req, res, next) => {
   res.status(404).send('<h1>404: Page Not Found<\h1.');
 });
 
+const uri = "mongodb+srv://testing:oeXeGlFbH8U1uEjA@guerrillascm-rk5d5.mongodb.net/GuerrillaSCM?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true
+  
+}).then(result =>{
+  app.listen(3000); //Listens for requests (asynchronous!)
+  console.log('API running on port: ' + 3000);
 
-app.listen(3000); //Listens for requests (asynchronous!)
-console.log('API running on port: ' + 3000);
+}).catch(err => {
+  console.log(err);
+});
