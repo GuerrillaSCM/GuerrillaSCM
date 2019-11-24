b9b85f91a5cb7df9bde0e2214b9893a601a60d14
Isaac <isaac.schultz@wsu.edu>
2019-11-23 16:38:07 -0800

    Adding meme route for testing

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,7 @@ const memeRoutes = require('./routes/meme');
 
 const mongoose = require('mongoose');
 
-const Survey = require('./models/Survey');
-const Question = require('./models/Question');
+
 
 const uri = "mongodb+srv://testing:oeXeGlFbH8U1uEjA@guerrillascm-rk5d5.mongodb.net/GuerrillaSCM?retryWrites=true&w=majority";
 mongoose.connect(uri, {
@@ -46,29 +45,3 @@ app.use((req, res, next) => {
 
 app.listen(3000); //Listens for requests (asynchronous!)
 console.log('API running on port: ' + 3000);
-
-
-//Example get function that tests using the schema
-app.get('/meme/:test', function (req, res) {
-  console.log("This bitch aint emtpy, anti-yeet!")
-
-  var meme1 = Survey({
-    title: "MemeTest" + req.params.test,
-    owner: "TestID",
-    published: false,
-    creationTime: new Date(),
-    questions: [{
-      type: "CommentBox",
-      questionID: 0,
-      prompt: "What do you think about memes?"
-    }],
-    trigger: null
-  });
-
-  meme1.save(function (err, insertion) {
-    if (err) return console.error(err);
-    console.log(insertion.title + " saved to meme collection.");
-  });
-
-  res.send('yeetus deletus')
-})
--- backend/routes/meme.js
+++ backend/routes/meme.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-router.use(function (req, res) {
-    console.log("This bitch aint emtpy, anti-yeet!")
+const Survey = require('../models/Survey');
+const Question = require('../models/Question');
+
+//Example get function that tests using the schema
+router.get(':test', function (req, res) {
+  console.log("This bitch aint emtpy, anti-yeet!")
+
+  var meme1 = Survey({
+    title: "MemeTest" + req.params.test,
+    owner: "TestID",
+    published: false,
+    creationTime: new Date(),
+    questions: [{
+      type: "CommentBox",
+      questionID: 0,
+      prompt: "What do you think about memes?"
+    }],
+    trigger: null
+  });
+
+  meme1.save(function (err, insertion) {
+    if (err) return console.error(err);
+    console.log(insertion.title + " saved to meme collection.");
+  });
+
   res.send('yeetus deletus')
 })
 
