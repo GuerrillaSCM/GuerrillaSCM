2a172034b3082199a5d9fa5a462f823b4e3e384c
kenzobanaag <kenzobanaag@ymail.com>
2019-12-12 09:57:38 -0800

    Added react router, and linked pages together

--- survey-mockup/src/Components/Modal/QuestionModal.js
+++ survey-mockup/src/Components/Modal/QuestionModal.js
@@ -32,7 +32,7 @@ export default function AddQuestionDialog(props) {
     const createNewId = require('uuid/v1');
 
     const itemId = () => {
-        console.log(props.questionId)
+        //console.log(props.questionId)
         if(props.questionId === undefined || props.questionId === null) {
             return createNewId();
         }
