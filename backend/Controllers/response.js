d33c5f83c27f53bcf701cd8b86a5fe1a0796d67b
Isaac <isaac.schultz@wsu.edu>
2020-02-08 18:26:24 -0800

    Spent way too much time refactoring.  now supports multiple questions being added

--- backend/Controllers/response.js
+++ backend/Controllers/response.js
@@ -1,12 +1,15 @@
 /*
     This file handles access to the database for the responses
+
+    It grabs all the models from the 'AnswerFactory' File, which is all the models for answer compiled into one answer type.
+
+    using the Answer.create(object) method, Answer can select the schema to use that is tailored to the input data.
 */
 
 const mongoose = require('mongoose');
 const Response = require('../models/SurveyResponse');
 const ObjectId = require('mongoose').Types.ObjectId;
-const Answer = require('../models/Answer'); //CHANGE THIS IN THE FUTURE. THIS IS NOT WHAT WE WANT, WANT IT TO BE ANSWER INSTEAD
-// const AnswerFactory = require('./AnswerFactory');
+const Answer = require('./AnswerFactory'); //This might be ghetto, we are grabbing an object from the controllers as if it is a model. It is effectively all the Anwser Models all compiled together
 
 /*
   Response ID controllers
@@ -23,43 +26,38 @@ exports.getResponseFromResponseID = (req, res, next) => {
     });
 }
 
-exports.postResponseGivenSurveyID = (req, res, next) => {
+exports.postResponseGivenSurveyID = async (req, res, next) => {
 
-  var oldBody = JSON.parse(JSON.stringify(req.body));
+  //////////////////////////////////////////////////////// REFACTOR THIS
+  var oldBody = JSON.parse(JSON.stringify(req.body)); //ghetto deep copy
   body = req.body; //refer to req.body so its more clear in the rest of the function.
 
   delete body['answers'];
 
   surveyResponse = Response(body);
   surveyResponse.surveyID = ObjectId(req.params.surveyID);
+  ///////////////////////////////////////////////////////
 
   if (oldBody.answers != null) { // if there are any answers in the response to add 
-    oldBody.answers.forEach(answer => {
-      // surveyResponse.answers.push(new AnswerFactory(answer));
-      Answer.create(answer).then((newAnswer) => {
-        surveyResponse.answers.push(newAnswer);
-        console.log(newAnswer);
-      }).catch((error) => {
-        console.log(error);
-      }).then(() => {
-        surveyResponse.answers.forEach(answer => {
-          answer.save((err, result) => {
-            if (err) {
-              return console.error(err);
-            }
-            console.log(result);
-            console.log("lkjdhlgkahdfjghxkodjghlksdfhghdsfhisdlkhigfukfdjcbn,jfxhgkldfxjopgihfhfdlkbhdflkjghflshgfkdhbsdufyhgoisudfogsdfpkghsldfkhgfdkuhgkdfhgsdfoiu");
-            console.log("saved question with id: " + answer._id);
-          });
-        });
-        surveyResponse.save(function (err, result) {
-          if (err) {
-            return console.error(err);
-          }
-          res.send(surveyResponse.surveyID + ' Inserted into database')
-          console.log(surveyResponse.surveyID + " saved to Survey collection.");
-        });
+    await Promise.all(oldBody.answers.map(async (answer) => { //go through each answer that needs to be saved. async function executes them all in parallel. waits until they all complete
+      newAnswer = await Answer.create(answer) //transform the answer into a document based on the answer schema models, 
+      surveyResponse.answers.push(newAnswer); //add that answer to the array that we will save to the database
+      await newAnswer.save((err, result) => { //save the answer, and wait until the operation completes to end this function call
+        if (err) {
+          return console.error(err);
+        } else {
+          return console.log("saved question with id: " + newAnswer._id);
+        }
       });
+    }));
+
+    surveyResponse.save((err, result) => { //save the main surveyResponse schema to the database
+      if (err) {
+        return console.error(err);
+      } else {
+        res.send(surveyResponse.surveyID + ' Inserted into database')
+        console.log(surveyResponse.surveyID + " saved to Survey collection.");
+      }
     });
   }
 }
